40e41864d23d6645dee6bbba6cfa0a4ec282045a
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-07-07 21:49:57 -0700

    Setup the test problem

--- LeetCode/src/Arrays/RemoveDuplicatesFromSortedArrray.java
+++ LeetCode/src/Arrays/RemoveDuplicatesFromSortedArrray.java
@@ -1,10 +1,15 @@
 package Arrays;
 
+import java.util.Arrays;
 
 //https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
 //Problem #26 Remove Duplicates from Sorted Array
 
 
 public class RemoveDuplicatesFromSortedArrray {
-	
+	public static void main(String[] args) {
+		int[] array = {1, 1, 2, 2, 3, 3, 3, 4, 5, 6, 7, 8, 9, 9, 10};
+		
+		System.out.println(Arrays.toString(array));
+	}
 }
