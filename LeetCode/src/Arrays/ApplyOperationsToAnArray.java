62ed8e923f59deb5fbd66d195b9b63091fb9b162
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-07-07 22:09:32 -0700

    Add a new Leetcode problem

---
+++ LeetCode/src/Arrays/ApplyOperationsToAnArray.java
@@ -0,0 +1,11 @@
+package Arrays;
+import java.util.Arrays;
+
+// https://leetcode.com/problems/apply-operations-to-an-array/description/
+// 2460. Apply Operations to an Array
+
+public class ApplyOperationsToAnArray {
+	public static void main(String[] args) {
+		
+	}
+}
