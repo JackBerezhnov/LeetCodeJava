0e67c6870d50005edd1f67402b142abf84c82f64
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-07-07 22:01:37 -0700

    Working on solution

--- LeetCode/src/Arrays/RemoveDuplicatesFromSortedArrray.java
+++ LeetCode/src/Arrays/RemoveDuplicatesFromSortedArrray.java
@@ -10,6 +10,16 @@ public class RemoveDuplicatesFromSortedArrray {
 	public static void main(String[] args) {
 		int[] array = {1, 1, 2, 2, 3, 3, 3, 4, 5, 6, 7, 8, 9, 9, 10};
 		
+		int j = 0;
+		for(int i = 0; i < array.length; i++) {
+			if (array[i] != array[j]) {
+				j += 1;
+				array[j] = array[i];
+			}
+		}
+		//It should return the length for a new array
+		j += 1;
+		System.out.println(j);
 		System.out.println(Arrays.toString(array));
 	}
 }
